3d4cfe176f7b0c85c578ca426c2a47b558228b39
kimwooglae <kimwooglae@gmail.com>
2020-07-30 09:01:01 +0900

    css test

--- work-2/css-test/src/App.js
+++ work-2/css-test/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button2';
+import Box from './Box1'
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button size="big" />
+      <Button size="small" />
+      <Box size="big" />
+      <Box size="small" />
     </div>
   );
 }
-
-export default App;
