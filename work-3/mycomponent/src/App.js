22b5a2702474b572b8e7d34f18fd221bfe039e3d
kimwooglae <kimwooglae@gmail.com>
2020-08-08 11:18:24 +0900

    virtual dom

--- work-3/mycomponent/src/App.js
+++ work-3/mycomponent/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import MyComponent from './MyComponent';
 
 function App() {
+
+  const elem = <a href='http://google.com'>click here</a>;
+  console.log(elem);
+
+  const elem1 = <h1>제 나이는 {40+4}<b>살</b> 입니다.</h1>;
+  console.log(elem1);
+
+  const elem2 = <MyComponent />;
+  console.log(elem2);
+
   return (
     <div className="App">
       <MyComponent />
+      <div id="root1"></div>
     </div>
   );
 }
 
+let second = 0;
+function update() {
+  second++;
+  const element = (
+    <div>
+      <h1>안녕하세요~</h1>
+      <h2>지금까지 {second}초가 지났습니다.</h2>
+    </div>
+  )
+  ReactDOM.render(element, document.getElementById('root1'));
+}
+
+setInterval(update, 1000);
 export default App;
