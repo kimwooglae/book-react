c7b5337c9b095e92cd609eb5e6299e0d76f54623
kimwooglae <kimwooglae@gmail.com>
2020-08-17 09:26:54 +0900

    provider

--- work-3/context/src/App.js
+++ work-3/context/src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const UserContext = React.createContext({username: '', helloCount: 0});
+const SetUserContext = React.createContext(() => {})
+const ThemeContext = React.createContext('dark');
 
 function App() {
+  const [user, setUser] = useState({username : 'mikke', helloCount: 0});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeContext.Provider value="dark">
+        <SetUserContext.Provider value={setUser}>
+          <UserContext.Provider value={user}>
+            <div>상단 메뉴</div>
+            <Profile />
+            <div>하단 메뉴</div>
+          </UserContext.Provider>
+        </SetUserContext.Provider>
+      </ThemeContext.Provider>
+      <input type="text" value={user.username} onChange={e => setUser({username: e.target.value})} />
     </div>
   );
 }
 
 export default App;
+
+// function Profile() {
+//   return (
+//     <div>
+//       <Greeting/>
+//     </div>
+//   );
+// }
+
+const Profile = React.memo(() => {
+  return (
+    <div>
+      <Greeting></Greeting>
+    </div>
+  )
+})
+
+function Greeting() {
+  return (
+    <ThemeContext.Consumer>
+      {theme => (
+        <SetUserContext.Consumer>
+          {setUser => (
+            <UserContext.Consumer>
+              {({username, helloCount}) => (
+                <div>
+                  <p style={{color: theme === 'dark' ? 'gray' : 'green'}}>{`${username}님 안녕하세요. `}</p>
+                  <p>{`인사 횟수: ${helloCount}`}</p>
+                  <button onClick={() =>
+                    setUser({username, helloCount:helloCount + 1})}> 인사 하기
+                  </button>
+                </div>
+              )}
+            </UserContext.Consumer>
+          )}
+        </SetUserContext.Consumer>
+      )}
+    </ThemeContext.Consumer>
+  )
+}
