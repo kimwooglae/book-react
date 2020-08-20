e95b61069171d4a9b6dfe0c5e5bace5444a5e359
kimwooglae <kimwooglae@gmail.com>
2020-08-20 21:55:44 +0900

    render 속성값

--- work-3/practical1/src/App.js
+++ work-3/practical1/src/App.js
@@ -1,11 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 function App() {
   return (
-    <div></div>
+    <div><MyComponent1 /></div>
   );
 }
 
 export default App;
 
+class MyComponent extends React.Component {
+  state = {
+    text: ''
+  };
 
+  divRef = React.createRef();
+  componentDidUpdate() {
+    const div = this.divRef.current;
+    const rect = div.getBoundingClientRect();
+    // console.log("componentDidUpdate", div.scrollWidth, rect.width);
+    if( div.scrollWidth > rect.width) {
+      alert('스크롤이 가능합니다.');
+    }
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+  onChange = event => {
+    const text = event.target.value;
+    // console.log("onChange", text);
+    this.setState({text});
+  };
+  render() {
+    const { text } = this.state;
+    // console.log("render", text);
+    return (
+      <React.Fragment>
+        <input onChange={this.onChange} value={text} />
+        <div ref={this.divRef} style={{width:1000, height:100, backgroundColor:'gray', overflow: 'scroll'}}>
+          {text}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+
+class MyComponent1 extends Component {
+  state = {
+    count: 0
+  };
+
+  onClickHello = e => {
+    e.preventDefault();
+    alert('hello world');
+  }
+
+  onClickDec = () => {
+    const {count} = this.state;
+    console.log("onclickDec", count);
+    this.setState({count:count-1});
+  };
+
+  onClickInc = () => {
+    const {count} = this.state;
+    console.log("onclickInc", count);
+    this.setState({count:count+1});
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onClickHello}>hello</button>
+        <button onClick={this.onClickDec}>dec</button>
+        <button onClick={this.onClickInc}>inc</button>
+      </div>
+    )
+  }
+}
---
+++ work-3/practical1/src/MouiseTracer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class MouseTracer extends React.Component {
+    state = {
+        x: null,
+        y: null
+    };
+    onMouseMove = e => {
+        this.setState({
+            x: e.clientX,
+            y: e.clientY
+        });
+    };
+    render() {
+        const { children } = this.props;
+        const { x, y } = this.state;
+        return <div onMouseMove={this.onMouseMove}>{children({x, y})}</div>
+    }
+}
+
+
+export default function MyComonent() {
+    return (
+        <MouseTracer >{({x, y }) => <p> {`(x, y): (${x}, ${y})`}</p>}</MouseTracer>
+    )
+}
