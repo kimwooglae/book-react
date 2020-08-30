3604d3aadba408b6e604d0d2ccc9aa024d1a983d
kimwooglae <kimwooglae@gmail.com>
2020-08-31 06:56:31 +0900

    redux

--- work-4/react-redux/src/index.js
+++ work-4/react-redux/src/index.js
@@ -5,16 +5,31 @@ import * as serviceWorker from './serviceWorker';
 import TimelineMain from './timeline/container/TimelineMain';
 import FriendMain from './friend/container/FriendMain';
 
+import store from './common/store';
+import { Provider } from 'react-redux';
+
 ReactDOM.render(
-  <div>
-    <FriendMain />
-    <TimelineMain />
-  </div>
-,
-  document.getElementById('root')
+  <Provider store={store}>
+    <div>
+      <FriendMain />
+      <TimelineMain />
+    </div>
+  </Provider>,
+  document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <div>
+//     <FriendMain />
+//     <TimelineMain />
+//   </div>
+// ,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
