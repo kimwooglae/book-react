cd34327df020db73a058fb521751324446b042c7
kimwooglae <kimwooglae@gmail.com>
2020-09-01 05:06:40 +0900

    useSelector

--- work-4/react-redux/src/friend/container/FriendMain.js
+++ work-4/react-redux/src/friend/container/FriendMain.js
@@ -6,9 +6,25 @@ import { addFriend } from '../state';
 import FriendList from '../component/FriendList';
 
 import { useSelector, useDispatch } from 'react-redux';
+import * as actions from '../state';
+
+import NumberSelect from '../component/NumberSelect';
+import { MAX_AGE_LIMIT, MAX_SHOW_LIMIT } from '../common';
 
 export default function FriendMain() {
-    const friends = useSelector(state => state.friend.friends);
+    const [
+        ageLimit, showLimit, friendsWithAgeLimit, friendsWithAgeShowLimit
+    ] = useSelector(state => {
+        const {friends, ageLimit, showLimit } = state.friend;
+        const friendsWithAgeLimit = friends.filter(
+            friend => friend.age <= ageLimit
+        );
+        return [
+            ageLimit, showLimit, friendsWithAgeLimit, friendsWithAgeLimit.slice(0, showLimit)
+        ]
+    })
+
+    // const friends = useSelector(state => state.friend.friends);
     const dispatch = useDispatch();
     // const [, forceUpdate] = useReducer(v => v + 1, 0);
     // useEffect(() => {
@@ -30,9 +46,27 @@ export default function FriendMain() {
     console.log("FriendMain render");
     // const friends = store.getState().friend.friends;
     return (
+        // <div>
+        //     <button onClick={onAdd}>친구 추가</button>
+        //     <FriendList friends={friends} />
+        // </div>
         <div>
             <button onClick={onAdd}>친구 추가</button>
-            <FriendList friends={friends} />
+            <NumberSelect onChange={v => dispatch(actions.setAgeLimit(v))}
+                value={ageLimit}
+                options={AGE_LIMIT_OPTIONS} 
+                postfix="세 이하만 보기" 
+            />
+            <FriendList friends={friendsWithAgeLimit} />
+            <NumberSelect onChange={v => dispatch(actions.setShowLimit(v))}
+                value={showLimit}
+                options={SHOW_LIMIT_OPTIONS}
+                postfix="명 이하만 보기 (연령 제한 적용)"
+            />
+            <FriendList friends={friendsWithAgeShowLimit} />
         </div>
     );
 }
+
+const AGE_LIMIT_OPTIONS = [15, 20, 25, MAX_AGE_LIMIT];
+const SHOW_LIMIT_OPTIONS = [2, 4, 6, MAX_SHOW_LIMIT];
