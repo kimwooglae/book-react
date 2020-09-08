ec416e83f61a24867466ea0fe71970d926a04672
kimwooglae <kimwooglae@gmail.com>
2020-09-09 07:24:39 +0900

    webpack split

---
+++ work-5/webpack-tree-shaking/src/index.js
@@ -0,0 +1,14 @@
+// esm 
+// import {func1} from './util_esm';
+
+// commonjs
+// import {func1} from './util_commonjs';
+
+// func1();
+
+// dynamic loading
+// import('./util_esm').then(util => util.func1());
+
+import { file } from 'lodash-es';
+const arr = [1, 2, 3];
+fill(arr, 'a');
