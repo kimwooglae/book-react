998d92123b3041a03a0ca0895a5ca3210b58a024
kimwooglae <kimwooglae@gmail.com>
2020-09-11 07:28:27 +0900

    import promise

--- work-5/webpack-split/src/index3.js
+++ work-5/webpack-split/src/index3.js
@@ -3,19 +3,30 @@ debugger;
 function myFunc() {
     import('./util').then(({ add }) => 
         import('lodash').then(({ default: _ }) => 
-            console.log('value', _.fill([1, 2, 3], add(10, 20)))
+            console.log('myFunc value', _.fill([1, 2, 3], add(10, 20)))
         )
     )
 }
 
 myFunc();
 
-// function myFunc1() {
-//     import('./util').then(function({ add }) { 
-//         return import('lodash').then(function({ default: _ }) { 
-//             return console.log('value', _.fill([1, 2, 3], add(10, 20)));
-//         })
-//     })
-// }
+function myFunc1() {
+    import('./util').then(function({ add }) { 
+        return import('lodash').then(function({ default: _ }) { 
+            console.log('myFunc1 value', _.fill([1, 2, 3], add(10, 20)));
+        })
+    })
+}
 
-// myFunc1();
+myFunc1();
+
+
+async function myFunc2() {
+    const [{ add }, { default: _}] = await Promise.all([
+        import("./util"),
+        import("lodash")
+    ]);
+    console.log('myFunc2 value', _.fill([1, 2, 3], add(10, 20)));
+}
+
+myFunc2();
