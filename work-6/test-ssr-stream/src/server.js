28c605d678d23d36db96cd455e74fb7d9be51ba6
kimwooglae <kimwooglae@gmail.com>
2020-09-14 07:57:34 +0900

    stream

--- work-6/test-ssr-stream/src/server.js
+++ work-6/test-ssr-stream/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-import { renderToString } from 'react-dom/server';
+import { renderToNodeStream } from 'react-dom/server';
 import React from 'react';
 import App from './App';
 import * as url from 'url';
@@ -24,6 +24,10 @@ for(const page of prerenderPages) {
     prerenderHtml[page] = pageHtml;
 }
 
+const html = fs
+    .readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf8')
+    .replace('__STYLE_FROM_SERVER__', '');
+
 app.use('/dist', express.static('dist'));
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 app.get('*', (req, res) => {
@@ -38,13 +42,42 @@ app.get('*', (req, res) => {
     const page = parsedUrl.pathname ? parsedUrl.pathname.substr(1) : 'home';
 
     const initialData = {page};
-    const pageHtml = prerenderPages.includes(page)
-        ? prerenderHtml[page]
-        : renderPage(page);
+    const isPrerender = prerenderPages.includes(page);
+    const result = (isPrerender ? prerenderHtml[page] : html).replace(
+        '__DATA_FROM_SERVER__',
+        JSON.stringify(initialData)
+    );
 
-    const result = pageHtml
-        .replace('__DATA_FROM_SERVER__', JSON.stringify(initialData));
-    ssrCache.set(cacheKey, result);
-    res.send(result);
+    if(isPrerender) {
+        ssrCache.set(cacheKey, result);
+        res.send(result);
+    } else {
+        const ROOT_TEXT = '<div id="ROOT">';
+        const prefix = result.substr(
+            0,
+            result.indexOf(ROOT_TEXT) + ROOT_TEXT.length
+        );
+        const postfix = result.substr(prefix.length);
+        res.write(prefix);
+        const sheet = new ServerStyleSheet();
+        const reactElement = sheet.collectStyles(<App page={page} />);
+        const renderStream = sheet.interleaveWithNodeStream(renderToNodeStream(reactElement));
+        renderStream.pipe(
+            res,
+            {end: false}
+        );
+        renderStream.on('end', () => {
+            res.end(postfix);
+        });
+    }
+
+    // const pageHtml = prerenderPages.includes(page)
+    //     ? prerenderHtml[page]
+    //     : renderPage(page);
+
+    // const result = pageHtml
+    //     .replace('__DATA_FROM_SERVER__', JSON.stringify(initialData));
+    // ssrCache.set(cacheKey, result);
+    // res.send(result);
 });
 app.listen(3000);
