d12f3be5377348ad53f0e1f46c0ac07951dc9551
kimwooglae <kimwooglae@gmail.com>
2020-09-14 07:45:08 +0900

    stream

---
+++ work-6/test-ssr-stream/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+// ReactDom.render(<App page="home" />, document.getElementById('root'));
+ReactDom.hydrate(<App page="home" />, document.getElementById('root'));
